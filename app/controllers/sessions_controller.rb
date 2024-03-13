0fa259c1b7d9e68867132279f88e55b839fbd903
Reuben Tochukwu Aniekwu <reuben.aniekwu@kellogg.northwestern.edu>
2024-03-13 02:09:48 +0000

    completed work on controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @User = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome to your Journal"
+        redirect_to "/places"
+      else
+        flash["notice"] = "Please Login"
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Please Login"
+      redirect_to "/login"
+    end
   end
 
   def destroy
