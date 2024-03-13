01515064afc11db12ad9c21e13f1ab86cbe8115d
Reuben Tochukwu Aniekwu <reuben.aniekwu@kellogg.northwestern.edu>
2024-03-13 03:04:37 +0000

    Making progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,23 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @User = User.find_by({"email" => params["email"]})
+    @User = User.find_by({"username" => params["username"]})
     if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "Welcome to your Journal"
-        redirect_to "/places"
+        redirect_to "/"
       else
-        flash["notice"] = "Please Login"
+        flash["notice"] = "Invalid login credentials"
         redirect_to "/login"
       end
     else
-      flash["notice"] = "Please Login"
+      flash["notice"] = "Invalid login credentials"
       redirect_to "/login"
     end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye"
+    redirect_to "/login"
   end
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    if User.find_by({"email" => params["email"]}) == nil
+    if User.find_by({"username" => params["username"]}) == nil
       @user = User.new
       @user["first_name"] = params["first_name"]
       @user["last_name"] = params["last_name"]
       @user["email"] = params["email"]
       @user["password"] = BCrypt::Password.create(params["password"])
       @user.save
-      redirect_to "/"
+      redirect_to "/places"
     else
-      flash["notice"] = "This Email has been taken"
+      flash["notice"] = "This Username has been taken"
       redirect_to "/users/new"
     end
   end
